785c7578bba4752b4ea8bb0241deea16dbeba62a
서지민 <sjm1172@gmail.com>
2025-10-27 20:52:02 +0900

    Delete src/main directory

--- src/main/java/com/mysite/sbb/study/repository/StudyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mysite.sbb.study.repository;
-
-import com.mysite.sbb.study.entity.Study;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyRepository extends JpaRepository<Study, Long> {
-
-}
