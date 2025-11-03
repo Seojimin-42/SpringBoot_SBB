ff3e18973609e68cca44ebda48a4931a3f17a8e9
서지민 <sjm1172@gmail.com>
2025-11-03 20:24:00 +0900

    Add files via upload

--- sbb/src/main/java/com/mysite/sbb/config/SecurityConfig.java
+++ sbb/src/main/java/com/mysite/sbb/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,7 +20,7 @@ public class SecurityConfig {
         http.logout(Customizer.withDefaults());
 
         http.authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/css/**", "/js/**").permitAll()
+                .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
                 .requestMatchers("/").permitAll()
                 .requestMatchers("/member/**").permitAll() // 권한 허용
                 .requestMatchers("/question/**").permitAll() // 권한 허용
@@ -26,6 +28,11 @@ public class SecurityConfig {
                 .anyRequest().authenticated());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- sbb/src/main/java/com/mysite/sbb/member/controller/MemberController.java
+++ sbb/src/main/java/com/mysite/sbb/member/controller/MemberController.java
@@ -1,19 +1,58 @@
 package com.mysite.sbb.member.controller;
 
 import com.mysite.sbb.member.dto.MemberDto;
+import com.mysite.sbb.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
-    @GetMapping("/create")
-    public String create(Model model){
+    private final MemberService memberService;
+
+    @GetMapping("/signup")
+    public String signup(Model model){
         model.addAttribute("memberDto", new MemberDto());
         return "member/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid MemberDto memberDto, BindingResult bindingResult,
+                         Model model){
+
+        if(bindingResult.hasErrors()){
+            return "member/signup";
+        }
+
+        if(!memberDto.getPassword1().equals(memberDto.getPassword2())){
+            bindingResult.rejectValue("password2", "passwordIncorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+            return "member/signup";
+        }
+
+        try{
+            memberService.create(memberDto);
+        } catch (DataIntegrityViolationException e){
+            log.info("========================== 회원 가입 실패 : 이미 등록된 사용자입니다.");
+            model.addAttribute("errorMessage", "이미 등록된 사용자입니다.");
+            return "member/signup";
+        } catch (Exception e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/signup";
+        }
+
+        return "redirect:/";
     }
 
 }
